78afbd5a7d4b6be3626e324b4d6d1ded3358f6cc
Your Name <you@example.com>
2022-03-12 14:34:52 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -94,12 +94,12 @@ Vector * crearVector(int n) {
 }
 
 /*
-Ejercicio 5a.
+Ejercicio 5.
 Programe la función void asignarValor(Vector * v, int i, int valor), 
 la cual asigna el valor a la posición i del vector v.
 */
 void asignarValor(Vector * v, int i, int valor) {
-
+   v->datos[i]=valor;
 }
 
 /*
